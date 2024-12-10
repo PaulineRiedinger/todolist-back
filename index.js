7065c1d5ef676676d60c3120970bf3d7d54e6958
Pauline Riedinger <levron.pauline@gmail.com>
2024-12-10 14:34:53 +0100

    Create homepage route, handle 404 errors, and add dotenv for environment variables

---
+++ index.js
@@ -0,0 +1,42 @@
+// Importer Express pour créer le serveur
+const express = require("express");
+// Importer Mongoose pour interagir avec la base de données
+const mongoose = require("mongoose");
+// Importer CORS pour gérer les requêtes externes
+const cors = require("cors");
+// Importer dotenv pour gérer les variables d'environnement
+require("dotenv").config();
+
+// Création du serveur
+const app = express();
+
+// Utilisation d'express.json pour récupérer des body dans les routes
+app.use(express.json());
+
+// Utilisation de cors
+app.use(cors());
+
+// Connexion à la base de données MongoDB
+mongoose.connect("mongodb://localhost:27017/listaholic");
+
+// Créer une route GET d'accueil
+app.get("/", (req, res) => {
+  console.log("Route d'accueil atteinte");
+  res.status(200).json({ message: "Bienvenue sur Listaholic" });
+});
+
+// Route test
+app.get("/test", (req, res) => {
+  console.log("Route test atteinte");
+  res.status(200).json({ message: "Test réussi" });
+});
+
+// Créer une route pour gérer toutes les routes inconnues
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "Route introuvable" });
+});
+
+// Faire tourner le serveur
+app.listen(3000, () => {
+  console.log(`Serveur démarré 🗒️`);
+});
